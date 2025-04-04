673c28556e4401327e9580ae293cd53a1a2d4049
Aran Fononi <aaronfononi@gmail.com>
2025-04-04 15:05:18 +0330

    Initial Commit

---
+++ Fruits SwiftUI MasterClass/Fruits_SwiftUI_MasterClassApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fruits_SwiftUI_MasterClassApp.swift
+//  Fruits SwiftUI MasterClass
+//
+//  Created by Aran Fononi on 4/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Fruits_SwiftUI_MasterClassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
