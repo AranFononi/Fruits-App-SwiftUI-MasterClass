fc3ed310ac3c64c2e89b2baa26499942e682c64f
Aran Fononi <aaronfononi@gmail.com>
2025-04-04 17:17:35 +0330

    Create SourceLinkView and add to FruitDetailView

---
+++ Fruits SwiftUI MasterClass/View/SourceLinkView.swift
@@ -0,0 +1,26 @@
+//
+//  SourceLinkView.swift
+//  Fruits SwiftUI MasterClass
+//
+//  Created by Aran Fononi on 4/4/25.
+//
+
+import SwiftUI
+
+struct SourceLinkView: View {
+    var body: some View {
+        GroupBox() {
+            HStack {
+                Text("Content Source")
+                Spacer()
+                Link("Wikipedia", destination: URL(string: "https://wikipedia.com")!)
+                Image(systemName: "arrow.up.right.square")
+            }
+            .font(.footnote)
+        }
+    }
+}
+
+#Preview {
+    SourceLinkView()
+}
