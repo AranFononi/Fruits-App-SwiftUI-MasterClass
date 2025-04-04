839ae4188d8c58d00c54f6af5a46200be0a334c3
Aran Fononi <aaronfononi@gmail.com>
2025-04-04 15:26:10 +0330

    Add elements to FruitCardView

--- Fruits SwiftUI MasterClass/View/FruitCardView.swift
+++ Fruits SwiftUI MasterClass/View/FruitCardView.swift
@@ -12,20 +12,38 @@ struct FruitCardView: View {
     
     // MARK: - Body
     var body: some View {
-        VStack(spacing: 20) {
-            // Fruit Image
-            Image("blueberry")
-                .resizable()
-                .scaledToFit()
-                .shadow(color: .black.opacity(0.15),radius: 8, x: 6, y: 8)
-            // Fruit Title
-            
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            // Fruit Headline
-            
-            // Fruit Start
-            
-        } //: VStack
+        ZStack {
+            VStack(spacing: 20) {
+                
+                // Fruit Image
+                Image("blueberry")
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(color: .black.opacity(0.15),radius: 8, x: 6, y: 8)
+                
+                // Fruit Title
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                    .foregroundStyle(.white)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .shadow(color: .black.opacity(0.15),radius: 2,x: 2,y: 2)
+                
+                // Fruit Headline
+                Text("Blueberries are sweet, nutritious, and widely fruit all over the world.")
+                    .foregroundStyle(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 16)
+                    .frame(maxWidth: 480)
+                
+                // Fruit Start
+                
+                
+            } //: VStack
+        } //: ZStack
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+        .background(LinearGradient(colors: [.colorBlueberryLight, .colorBlueberryDark], startPoint: .top, endPoint: .bottom))
+        .clipShape(RoundedRectangle(cornerRadius: 20))
+        
     }
 }
 
