a075d264b408260da0e4fab17edcec697faf1dfd
Aran Fononi <aaronfononi@gmail.com>
2025-04-04 15:43:30 +0330

    Create DataModel

---
+++ Fruits SwiftUI MasterClass/Model/FruitModel.swift
@@ -0,0 +1,20 @@
+//
+//  FruitModel.swift
+//  Fruits SwiftUI MasterClass
+//
+//  Created by Aran Fononi on 4/4/25.
+//
+
+import SwiftUI
+
+// MARK: - Fruits Data Model
+
+struct FruitModel: Identifiable {
+    var id = UUID()
+    var title: String
+    var headline: String
+    var image: String
+    var gradientColors: [Color]
+    var description: String
+    var nutrition: [String]
+}
